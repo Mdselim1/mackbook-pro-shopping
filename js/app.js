54d73f62714a631205d5387be18da1b8672afad9
MDSELIM1 <selimakodo58@gmail.com>
2021-08-21 04:04:26 +0600

    add

--- js/app.js
+++ js/app.js
@@ -3,32 +3,33 @@ function valuePrint(idpara, value) {
     return mainValue;
 }
 
+function totalFind(cost) {
+    const averageCost = document.getElementById('total-price');
+    averageCost.innerText = cost;
+    document.getElementById('coupon-total').innerText = cost;
+}
+
 function similarAdd(valueOne) {
     const storage256gb = document.getElementById('storage-cost').innerText;
     const deliveryCost = document.getElementById('delivery-cost').innerText;
     const baseCost = document.getElementById('base-price').innerText;
     const totalCost = parseInt(valueOne) + parseInt(storage256gb) + parseInt(deliveryCost) + parseInt(baseCost);
-    const averageCost = document.getElementById('total-price');
-    averageCost.innerText = totalCost;
-    document.getElementById('coupon-total').innerText = totalCost;
+    totalFind(totalCost);
+
 }
 function similarAddTwo(valueTwo) {
     const storage256gb2 = document.getElementById('memory-cost').innerText;
     const deliveryCost2 = document.getElementById('delivery-cost').innerText;
     const baseCost2 = document.getElementById('base-price').innerText;
     const totalCost2 = parseInt(valueTwo) + parseInt(storage256gb2) + parseInt(deliveryCost2) + parseInt(baseCost2);
-    const averageCost2 = document.getElementById('total-price');
-    averageCost2.innerText = totalCost2;
-    document.getElementById('coupon-total').innerText = totalCost2;
+    totalFind(totalCost2);
 }
 function similarAddThree(valueThree) {
     const storage256gb3 = document.getElementById('memory-cost').innerText;
     const deliveryCost3 = document.getElementById('storage-cost').innerText;
     const baseCost3 = document.getElementById('base-price').innerText;
     const totalCost3 = parseInt(valueThree) + parseInt(storage256gb3) + parseInt(deliveryCost3) + parseInt(baseCost3);
-    const averageCost3 = document.getElementById('total-price');
-    averageCost3.innerText = totalCost3;
-    document.getElementById('coupon-total').innerText = totalCost3;
+    totalFind(totalCost3);
 }
 
 document.getElementById('memory-8gb-price').addEventListener('click', function () {
@@ -63,11 +64,14 @@ document.getElementById('delivery-prime').addEventListener('click', function () 
 document.getElementById('promo-btn').addEventListener('click', function () {
     const promoValueField = document.getElementById('promo-value');
     const promoValue = promoValueField.value;
+    promoValueField.value = '';
     const totalValue = document.getElementById('coupon-total');
     const totalNumber = parseInt(totalValue.innerText);
     if (promoValue == 'stevekaku') {
         const totalNumberPer = 20 / 100;
         const findNumber = parseInt(totalNumber * totalNumberPer);
         totalValue.innerText = totalNumber - findNumber;
+    } else {
+        document.getElementById('error').innerText = 'Please give right coupon';
     }
 })
